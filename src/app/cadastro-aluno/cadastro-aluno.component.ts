4e0c964ffcd8e7335bfa787201b2fe37c388a2c0
gabrielagabriela <gabrielaa.slv@gmail.com>
2024-07-18 17:42:12 -0300

    Ex 04

--- src/app/cadastro-aluno/cadastro-aluno.component.ts
+++ src/app/cadastro-aluno/cadastro-aluno.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro-aluno',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './cadastro-aluno.component.html',
   styleUrl: './cadastro-aluno.component.scss'
 })
-export class CadastroAlunoComponent {
+export class CadastroAlunoComponent implements OnInit {
 
+  constructor(private router: Router) {}
+  cadastroForm!: FormGroup;
+  alunoCadastrado: any;
+
+  ngOnInit(): void {
+      this.criarForm();
+  }
+
+  criarForm(){
+    this.cadastroForm = new FormGroup({
+      nome: new FormControl('', Validators.required),
+      cpf: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
+      celular: new FormControl('', Validators.required),
+      curso: new FormControl('', Validators.required)
+
+    })
+  }
+
+  salvar(){
+    if(this.cadastroForm.valid){
+      this.alunoCadastrado = this.cadastroForm.value;
+      console.log("Aluno cadastrado:", this.alunoCadastrado);
+      this.router.navigate(['/alunos']);
+    } else {
+      this.cadastroForm.markAllAsTouched()
+    }
+  }
 }
