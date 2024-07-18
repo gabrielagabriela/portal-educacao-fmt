b9df1e583fd9ff1b5de967cdb8a2dd0d0a206254
gabrielagabriela <gabrielaa.slv@gmail.com>
2024-07-18 16:08:36 -0300

    Ex 03

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  constructor(private router: Router) {}
+
+  loginFormModel = {
+    email: '',
+    senha: ''
+  }
+
+  onSubmit(){
+    if(this.loginFormModel.email && this.loginFormModel.senha){
+      console.log("redirecionar para a home")
+      this.router.navigate(['/home']);
+    } else{
+      console.log("preencha os campos")
+    }
+  }
+
+  esqueciSenha(){
+    alert("Processo de recuperação de senha envido para o e-mail cadastrado")
+  }
 }
